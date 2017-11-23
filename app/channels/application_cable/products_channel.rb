91fc41c51962c69ad36e8359da6b12e14a6d9cc0
nghia <hdn.hodacnghia.hdn@gmail.com>
2017-11-23 11:31:35 +0700

    config db postgre

--- app/channels/application_cable/products_channel.rb
+++ app/channels/application_cable/products_channel.rb
@@ -2,4 +2,7 @@ class ProductsChannel < ActionCable::Channel::Base
     def subscribed
       stream_from "products"
     end
-  end
+    def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+    end
+    end
