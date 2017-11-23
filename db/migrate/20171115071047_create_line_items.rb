6ca78caa3886fcb0937ce740e0f93bdfb334735d
nghia <hdn.hodacnghia.hdn@gmail.com>
2017-11-23 11:06:38 +0700

    final app1

--- db/migrate/20171115071047_create_line_items.rb
+++ db/migrate/20171115071047_create_line_items.rb
@@ -1,6 +1,5 @@
 class CreateLineItems < ActiveRecord::Migration[5.1]
   def change
-    drop_table :line_items
     create_table :line_items do |t|
       t.references :product, foreign_key: true
       t.belongs_to :cart, foreign_key: true
